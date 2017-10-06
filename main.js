c3dcee804db8991b89a7984ba1d362be5c0c618a
Arlo Shefte-Jacobs <arlo.sj3@gmail.com>
2017-10-06 16:27:12 -0600

    query working

--- main.js
+++ main.js
@@ -6,15 +6,31 @@ function showError(msg) {
 
 // Searches for books and returns a promise that resolves a JSON list
 function searchForBooks(term) {
-  
-  // TODO
-  fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
+
+  let btn = document.getElementById('search-btn');
+btn.onclick = function(){
+  // alert(document.getElementById('search-bar').value)
+  var searcher = document.getElementById('search-bar').value;
+  console.log(searcher)
+  fetch('https://www.googleapis.com/books/v1/volumes?q='+searcher)
 .then(function (response){
     return response.json();
 })
 .then(function (json){
     console.log(json);
 });
+}
+
+  // let searcher = document.getElementById('search-bar').value;
+  // console.log(searcher)
+  // TODO
+//   fetch('https://www.googleapis.com/books/v1/volumes?q=harry+potter')
+// .then(function (response){
+//     return response.json();
+// })
+// .then(function (json){
+//     console.log(json);
+// });
 }
 
 // Generate HTML and sets #results's contents to it
